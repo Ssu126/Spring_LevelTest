8b3770fa5c5fd87cdbc1385079e3c1e10402b62f
Suyoung <sueyoungkim69@gmail.com>
2026-08-02 20:47:24 +0900

    first commit

---
+++ src/main/java/com/example/Spring_LevelTest/SpringLevelTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.Spring_LevelTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLevelTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLevelTestApplication.class, args);
+    }
+}
